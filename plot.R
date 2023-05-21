71e9dc836f8dc884ef5dfbf4fe1282a0779720af
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-05-21 11:13:05 +0000

    plot tornado traces on top of map

--- plot.R
+++ plot.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(ggplot2)
 library(targets)
+library(tigris)
+library(janitor)
 
 tornados <- tar_read(tornados)
 
@@ -8,14 +10,29 @@ ky_tornados <- tornados |>
   filter(stf == 21)
   
 # most of the data doesn't have an end lat lon
-ky_tornados |>
-  filter(elat != 0) |>
+ky_tornados_td <- ky_tornados |> filter(elat != 0)
+
+kentucky_counties <- counties(state = "KY") %>%
+  clean_names()
+
+ky_landmarks <- landmarks("KY")
+ky_towns_to_plot <- c("Berea", "Bowling Green", "Lexington", "Louisville")
+ky_towns <- ky_landmarks %>% filter(FULLNAME %in% ky_towns_to_plot)
+
+kentucky_counties |>
   ggplot() +
-  geom_segment(
-    aes(
-      x = slon,
-      y = slat,
-      xend = elon,
-      yend = elat
-    )
-  )
+    geom_sf(fill = "white", color = "black") +
+    geom_segment(
+      data = ky_tornados_td,
+      arrow = arrow(length = unit(0.015, "npc")),
+      aes(
+        x = slon,
+        y = slat,
+        xend = elon,
+        yend = elat,
+        colour = as.factor(mag)
+      )
+    ) +
+    theme_void()
+
+
