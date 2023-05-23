d3ebee072ec5e753d5b970fd005f7df016dda3c3
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-05-23 16:20:49 +0000

    add state outline, facet by decade

--- plot.R
+++ plot.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(ggplot2)
 library(targets)
 
@@ -5,10 +6,27 @@ ky_counties <- tar_read(ky_counties)
 ky_tornadoes <- tar_read(ky_tornadoes)
 ky_towns <- tar_read(ky_towns)
 
+us_states <- tigris::states(class = "sf")
+kentucky <- us_states[us_states$STUSPS == "KY", ]
+
+# https://stackoverflow.com/questions/35352914/floor-a-year-to-the-decade-in-r
+floor_decade <- function(value){ return(value - value %% 10) }
+ky_tornadoes <- ky_tornadoes |>
+  filter(!is.na(mag)) |>
+  mutate(decade = floor_decade(yr))
+
 ggplot() +
-  geom_sf(data = ky_counties, fill = "white", color = "black", linewidth = 0.1) +
-  geom_sf(data = ky_tornadoes, aes(colour = as.factor(mag)),
+  geom_sf(data = kentucky, colour = "black") +
+  geom_sf(data = ky_counties, fill = "lightgrey", color = "black", linewidth = 0.1) +
+  geom_sf(data = ky_tornadoes,
+          aes(colour = as.factor(mag)),
+          linewidth = 1,
           arrow = arrow(angle = 45, ends = "last", type = "open", length = unit(0.05, "inches"))) +
-  #geom_sf(data = ky_towns) +
-  #geom_sf_text(data = ky_towns, aes(label = FULLNAME), nudge_x = -.2, nudge_y = -.2) +
-  theme_void()
+  theme_void() +
+  scale_color_brewer(palette = "Reds") +
+  #facet_wrap(~ decade) +
+  theme(legend.position = "bottom") +
+  guides(colour = guide_legend(nrow = 1, byrow = TRUE)) +
+  labs(
+    colour = "Intensity (EF-Scale)"
+  )
